3482b7d509112fe27be64efc226aa771f763d8a1
nadusumilli <nadusumilli@unomaha.edu>
2016-03-11 02:45:28 -0600

    Stock price Pulled and added

--- api/controllers/CustomerController.js
+++ api/controllers/CustomerController.js
@@ -21,8 +21,53 @@ module.exports = {
     	Customer.findOne(req.param('id')).populateAll().exec(function (err, customer) {
       		if (err) return next(err);
       		if (!customer) return next();
-      		res.view({
-        		customer: customer
+
+      		var http = require('http');
+
+      		function process_response(webservice_response, stock, callback){
+      			var webservice_data ="";
+      			webservice_response.on('error', function(e){
+      				//console.log(e.message);
+      				callback("Error: "+e.message);
+      			});
+
+      			webservice_response.on('data', function(chunck){
+      				webservice_data += chunck;
+      			});
+
+      			webservice_response.on('end', function(){
+      				stock_data = JSON.parse(webservice_data);
+      				stock.current_price = stock_data.LastPrice;
+      				//console.log(stock.symbol + '= $'+stock.current_price);
+      				callback();
+      			});
+      		};
+
+
+      		function get_current_price(stock, callback){
+      			console.log(stock.symbol);
+      			options = {
+      				host: 'dev.markitondemand.com',
+      				port: 80,
+      				path: '/MODApis/Api/v2/Quote/JSON?symbol=' + stock.symbol,
+      				method: 'GET'
+      			};
+
+      			var webservice_request = http.request(options, function(response){
+      				process_response(response, stock, callback)
+      			});
+      			webservice_request.end();
+
+      			//console.log(stock.symbol +'='+stock.current_price);
+      		};
+
+      		async.each(customer.stocks, get_current_price, function(err){
+      			if(err) console.log(err);
+      			//console.log('done');
+
+	      		res.view({
+	        		customer: customer
+	      		});
       		});
     	});
   	},
