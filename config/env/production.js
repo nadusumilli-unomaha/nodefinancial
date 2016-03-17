dd57b48e9b912c075bfec8042f4dae559a3b80c2
nadusumilli <nadusumilli@unomaha.edu>
2016-03-17 04:07:28 -0500

    add

--- config/env/production.js
+++ config/env/production.js
@@ -12,7 +12,7 @@
 
 module.exports = {
 
-  hookTimeout: 10, //30
+  hookTimeout: 60000, //30
   /***************************************************************************
    * Set the default database connection for models in the production        *
    * environment (see config/connections.js and config/models.js )           *
